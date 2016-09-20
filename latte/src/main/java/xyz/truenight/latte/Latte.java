70f1c438e14faec15a3235be7880249e28d46777
Mikhail Frolov <mikhail.frolov@tobox.com>
2016-09-20 15:19:11 +0300

    Updated access level for checkArgument() and checkNotNull().

--- latte/src/main/java/xyz/truenight/latte/Latte.java
+++ latte/src/main/java/xyz/truenight/latte/Latte.java
@@ -252,14 +252,14 @@ public class Latte {
         }
     }
 
-    public static <T> T checkNotNull(T obj) {
+    static <T> T checkNotNull(T obj) {
         if (obj == null) {
             throw new NullPointerException();
         }
         return obj;
     }
 
-    public static void checkArgument(boolean condition) {
+    static void checkArgument(boolean condition) {
         if (!condition) {
             throw new IllegalArgumentException();
         }
